0fade78e7d5d37245cc74a898f4b8b165560891b
howard-richwhole <79359287+howard-richwhole@users.noreply.github.com>
2021-05-06 16:31:32 +0800

    Update TypeLibLoader.cpp

--- src/TypeLibLoader.cpp
+++ src/TypeLibLoader.cpp
@@ -28,6 +28,7 @@ void TypeLibLoader::Load( const Nan::FunctionCallbackInfo< v8::Value >& info )
 	v8::Local< v8::Value > argv[ 1 ] = { Nan::New< v8::External >( typeLib.p ) };
 	v8::Local< v8::Function > cons = Nan::New< v8::Function >( TypeLib::constructor );
 	info.GetReturnValue().Set( cons->NewInstance( Nan::GetCurrentContext(), 1, argv ).ToLocalChecked() );
+		Nan::ThrowTypeError( "aaaaaaaaaaa" );
 }
 
 void TypeLibLoader::Init( v8::Local< v8::Object > exports )
